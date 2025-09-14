596a0279407534a773ffe030c5afa1d3c8150cc4
Pouyadevsecops <pouya.it.devops@gmail.com>
2025-09-14 17:15:17 +0330

    add multi host configs

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 data "vsphere_datacenter" "datacenter" {
-  name = "Mtyn DataCenter"
+  name = var.datacenter
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = var.hard_disk
+  for_each         = { for vm in var.vms : vm.name => vm }
+  name          = each.value.hard_disk
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
@@ -22,15 +23,23 @@ data "vsphere_virtual_machine" "template" {
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
+# one data lookup per VM host
+data "vsphere_host" "esxi" {
+  for_each      = { for vm in var.vms : vm.name => vm }
+  name          = each.value.host
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+# create VM on correct host
 resource "vsphere_virtual_machine" "vm" {
-  count            = length(var.vm_ip)
-  name             = "${var.resource_name}-${count.index}"
+  for_each         = { for vm in var.vms : vm.name => vm }
+  name             = each.value.name
   resource_pool_id = data.vsphere_compute_cluster.MTYN.resource_pool_id
-  datastore_id     = data.vsphere_datastore.datastore.id
+  datastore_id     = data.vsphere_datastore.datastore[each.key].id
+  host_system_id   = data.vsphere_host.esxi[each.key].id
 
   num_cpus = var.num_cpus
   memory   = var.memory
-
   guest_id = data.vsphere_virtual_machine.template.guest_id
 
   network_interface {
@@ -39,85 +48,65 @@ resource "vsphere_virtual_machine" "vm" {
   }
 
   disk {
-    label = "disk0"
-    size  = var.hard_disk_size
-    #size             = data.vsphere_virtual_machine.template.disks.0.size
-    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
-    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks[0].size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks[0].eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks[0].thin_provisioned
   }
 
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
     customize {
       linux_options {
-        host_name = "${var.host_name}-${count.index}"
+        host_name = each.value.name
         domain    = var.domain
-
       }
       network_interface {
-        ipv4_address = var.vm_ip[count.index]
+        ipv4_address = each.value.ip
         ipv4_netmask = 24
       }
       ipv4_gateway    = var.ipv4_gateway
       dns_server_list = var.dns_server_list
-
-
-
-    }
-  }
-}
-resource "time_sleep" "wait_1_min" {
-  depends_on = [vsphere_virtual_machine.vm]
-
-  create_duration = "1m"
-}
-
-resource "null_resource" "after" {
-  depends_on = [time_sleep.wait_1_min]
-  count      = length(var.vm_ip)
-  provisioner "remote-exec" {
-    inline = [
-      "echo '${var.ssh_password}' | sudo -S apt update",
-      #"echo '${var.ssh_password}' | sudo -S apt install lynis -y"
-    ]
-    connection {
-      type     = "ssh"
-      user     = var.ssh_user
-      password = var.ssh_password
-      host     = var.vm_ip[count.index]
-      timeout  = "10m"
     }
   }
 }
 
-resource "local_file" "ansible_inventory" {
-  filename = "${path.module}/inventory.ini"
+# wait for guest customization
+# resource "time_sleep" "wait_1_min" {
+#   depends_on      = [vsphere_virtual_machine.vm]
+#   create_duration = "1m"
+# }
 
-  content = <<EOT
-[cluster_nodes]
-%{for idx, ip in var.vm_ip~}
-node${idx + 1} ansible_host=${ip}
-%{endfor}
+# simple remote exec on each VM
+# resource "null_resource" "after" {
+#   for_each = vsphere_virtual_machine.vm
+#   depends_on = [time_sleep.wait_1_min]
 
-[all:vars]
-project_dir=/opt/project
-ansible_user=${var.ssh_user}
-ansible_ssh_pass=${var.ssh_password}
-ansible_become_pass=${var.ssh_password}
-EOT
-}
+#   provisioner "remote-exec" {
+#     inline = [
+#       "echo '${var.ssh_password}' | sudo -S apt update"
+#     ]
+#     connection {
+#       type     = "ssh"
+#       user     = var.ssh_user
+#       password = var.ssh_password
+#       host     = each.value.clone[0].customize[0].network_interface[0].ipv4_address
+#       timeout  = "10m"
+#     }
+#   }
+# }
 
+# run ansible playbook on each VM
+# resource "null_resource" "run_ansible" {
+#   for_each = vsphere_virtual_machine.vm
 
-resource "null_resource" "run_ansible" {
-  depends_on = [vsphere_virtual_machine.vm]
-  provisioner "local-exec" {
-    command = <<EOT
-      export ANSIBLE_HOST_KEY_CHECKING=False
-      ansible-playbook -i inventory.ini playbook.yml \
-        --user ${var.ssh_user} \
-        --extra-vars '{"ansible_ssh_pass": "${var.ssh_password}", "ansible_become_pass": "${var.ssh_password}"}' \
-        --connection=ssh --tags ${var.ansible_tags}
-    EOT
-  }
-}
-
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       export ANSIBLE_HOST_KEY_CHECKING=False
+#       ansible-playbook -i "${each.value.clone[0].customize[0].network_interface[0].ipv4_address}," add-user.yml \
+#         --user ${var.ssh_user} \
+#         --extra-vars '{"ansible_ssh_pass": "${var.ssh_password}", "ansible_become_pass": "${var.ssh_password}"}' \
+#         --connection=ssh
+#     EOT
+#   }
+# }
